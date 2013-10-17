1e6bdb2e150d2ce71245fcdfbdca9db71f26019e
mmaa94 <maggi123_940620@hotmail.com>
2013-10-17 12:24:08 -0500

    correcciones finale, notebook final

--- DesarrolloPunto1/module_chi2.py
+++ DesarrolloPunto1/module_chi2.py
@@ -4,7 +4,7 @@
 import sys
 import codecs
 import math
-
+import numpy as np
 """
 Módulo que contiene la función para calcular el Chi².
 Autores: María M. Ariza Acero y  César A. Quintana Cataño
@@ -25,23 +25,21 @@ def calcular_chi(teoricos, reales):
     n=len(teoricos)
 
     #Arreglo para guardar la resta en cada punto
-    restas=np.empty(n) 
+    restas=np.zeros(n) 
     
     #Arreglo para guardar el resultado de la resta elevado al cuadrado 
-    restas_2=np.empty(n)  
+    restas_2=np.zeros(n)  
  
     #Variable para guardar la suma de las restas elevadas al cuadrado
     suma=0 
  
     #Ciclo para calcular las restas 
     for i in range(n): 
- 
         restas[i]=teoricos[i]-reales[i] 
         restas_2[i]=pow(restas[i],2) 
 
     #Ciclo para calcular la suma de las restas
-    for i in rage(n): 
- 
+    for i in range(n): 
         suma=suma+restas_2[i] 
 
     #Calculo del chi²
